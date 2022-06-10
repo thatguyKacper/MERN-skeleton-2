cf74202b52f02133cd1aa1acdd69b0a67187a61a
Kacper <kacper.m.wojtowicz@gmail.com>
2022-06-10 13:31:42 +0200

    add error handler

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const { errorHandler } = require('./helpers/errorHandler');
 
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
 // ROUTES
 app.use('/api/users', userRoutes);
+
+app.use(errorHandler);
 
 module.exports = app;
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,22 +1,33 @@
-const getAllUsers = (req, res) => {
+// for not using try catch
+const asyncHandler = require('express-async-handler');
+
+const getAllUsers = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: 'get users',
   });
-};
-const createUser = (req, res) => {
+});
+
+const createUser = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error('bad request');
+  }
+
   res.status(200).json({
     message: 'create user',
   });
-};
-const updateUser = (req, res) => {
+});
+
+const updateUser = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: `update user ${req.params.id}`,
   });
-};
-const deleteUser = (req, res) => {
+});
+
+const deleteUser = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: `delete user ${req.params.id}`,
   });
-};
+});
 
 module.exports = { getAllUsers, createUser, updateUser, deleteUser };
